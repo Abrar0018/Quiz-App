0f014844c5e6d7e00a9b79f63b40d13f4564917d
user.abrar0018 <abrarahmadm1@gmail.com>
2022-11-13 23:25:21 +0500

    finished logic

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -4,21 +4,90 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     List<Question> questions = new ArrayList<Question>();
+    Question currentQuestion;
+    int correctAnswers = 0,incorrectAnswers=0,questionNumber=0;
+    List<RadioButton> optionRadios = new ArrayList<>();
+    TextView questionContentView;
+    TextView questionNumView;
+    Button nextBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
 
     @Override
     public void onClick(View view) {
+        RadioGroup radioButtonGroup = findViewById(R.id.radioGroup);
 
+        //get selected option
+        int radioButtonID = radioButtonGroup.getCheckedRadioButtonId();
+        View radioButton = radioButtonGroup.findViewById(radioButtonID);
+        int idx = radioButtonGroup.indexOfChild(radioButton)+1;
+
+        //check if the selected option is correct
+        if(idx == currentQuestion.correctOption)
+            correctAnswers++;
+        else
+            incorrectAnswers++;
+
+        //all questions have been answered
+        if(questionNumber==questions.size()-1)
+        {
+            questionNumView.setText(Integer.toString(correctAnswers));
+            return;
+        }
+
+        //last question is reached
+        if(questionNumber==questions.size()-2)
+            nextBtn.setText("Finish");
+
+
+
+        questionNumView.setText("Question # " + ++questionNumber);
+        setQuestion(questions.get(questionNumber));
+    }
+
+    public void init()
+    {
+        //add event listener to the nextBtn
+        nextBtn = findViewById(R.id.nextBtn);
+        nextBtn.setOnClickListener(this);
+        //get question content textview
+        questionContentView = findViewById(R.id.question);
+        //get all for option's radio btns
+        optionRadios.add(findViewById(R.id.option1));
+        optionRadios.add(findViewById(R.id.option2));
+        optionRadios.add(findViewById(R.id.option3));
+        optionRadios.add(findViewById(R.id.option4));
+        //get question number textview
+        questionNumView = findViewById(R.id.questionNum);
+        questionNumView.setText("Question # " + questionNumber);
+        //initialize questions list
+        createQuestions();
+        //set first question
+        setQuestion(questions.get(questionNumber));
+    }
+
+    public void setQuestion(Question question)
+    {
+        currentQuestion = question;
+        questionContentView.setText(question.content);
+        optionRadios.get(0).setText(question.option1);
+        optionRadios.get(1).setText(question.option2);
+        optionRadios.get(2).setText(question.option3);
+        optionRadios.get(3).setText(question.option4);
     }
 
     public void createQuestions()
