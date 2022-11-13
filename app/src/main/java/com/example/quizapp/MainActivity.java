63faf37af558de149e8636489911196256fefa9b
user.abrar0018 <abrarahmadm1@gmail.com>
2022-11-13 22:35:56 +0500

    created objects

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    List<Question> questions = new ArrayList<Question>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +20,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
 
     }
+
+    public void createQuestions()
+    {
+        questions.add(new Question("The term 'Islam' means",
+                "Submission","Peace","Fortitude","Thankfulness",1));
+        questions.add(new Question("Which of the following is not an essential part of Islamic belief?",
+                "Oneness of God","Day of Judgment","Sorcery","Prophets",3));
+        questions.add(new Question("The chapters of the Qur’an are known as",
+                "Surahs","Sunnahs","Shari’ah","Sufis",1));
+        questions.add(new Question("The word jihad means",
+                "Pilgrimage","To strive or struggle","Fasting","Prophecy",2));
+        questions.add(new Question("Which of the following is not one of the Five Pillars of Islam?",
+                "Fasting during the month of Ramadan",
+                "Jihad","Declaration of faith"," Prayer five times daily",1));
+    }
 }
 
 class Question{
@@ -25,13 +43,14 @@ class Question{
     public String option2;
     public String option3;
     public String option4;
-    public Question(String c,String o1,String o2,String o3,String o4 )
+    public int correctOption;
+    public Question(String c,String o1,String o2,String o3,String o4,int correct )
     {
         content = c;
         option1 = o1;
         option2 = o2;
         option3 = o3;
         option4 = o4;
+        correctOption = correct;
     }
-
 }
