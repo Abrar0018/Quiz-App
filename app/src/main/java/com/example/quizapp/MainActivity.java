034b462f33f3695919cb3ed8b01f6577b1109acb
user.abrar0018 <abrarahmadm1@gmail.com>
2022-11-13 22:21:45 +0500

    added question class

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
+
+class Question{
+    public String content;
+    public String option1;
+    public String option2;
+    public String option3;
+    public String option4;
+    public Question(String c,String o1,String o2,String o3,String o4 )
+    {
+        content = c;
+        option1 = o1;
+        option2 = o2;
+        option3 = o3;
+        option4 = o4;
+    }
+
 }
